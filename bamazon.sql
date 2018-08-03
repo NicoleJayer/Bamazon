281460d4f710e5cd3973bb58342811940512e0a8
Nicole Richardson <njrichardson95@gmail.com>
2018-08-03 13:14:43 -0400

    fixed some errors with the sql file, database now successfully created

--- bamazon.sql
+++ bamazon.sql
@@ -1,9 +1,11 @@
+DROP DATABASE IF EXISTS bamazon;
+
 CREATE DATABASE bamazon;
 
 USE bamazon;
 
 CREATE TABLE products (
-  item_id INTEGER NOT NULL AUTO_INCREMENT,
+  item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
   product_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(100) NOT NULL,
   price INTEGER,
@@ -11,32 +13,32 @@ CREATE TABLE products (
   PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("shampoo", "beauty", 10.00, 5);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("conditioner", "beauty", 15.00, 10);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("facial cleanser", "beauty", 20.00, 4);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("shirt", "fashion", 30.00, 20);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("pants", "fashion", 50.00, 15);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("shoes", "fashion", 100.00, 6);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("microwave", "household", 60.00, 10);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("windex", "household", 5.00, 15);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("sponge", "household", 3.00, 5);
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("rug", "household", 20.00, 10);
